1d3873902d5514f8652178da9c29aac89ec697c3
WilliamNietoA <williamnieto93@gmail.com>
2023-09-15 03:11:05 +0200

    fixing 3

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const path = require("path");
 const port = process.env.PORT || 3030;
 const app = express();
 
-app.use(express.static(publicPath));
+app.use(express.static("publicPath"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/home.html"));
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
 
 app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/register.html"));
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
 })
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/login.html"))
+    res.sendFile(path.resolve(__dirname, "/views/login.html"))
 })
 
 app.listen(port, () => console.log('Servidor corriendo ML-' + port)); 
