bc12fa3c09700c14e24e490035ca0867b76d0490
WilliamNietoA <williamnieto93@gmail.com>
2023-09-15 03:25:52 +0200

    fixing images

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 3030;
 const app = express();
+const publicPath = path.resolve(__dirname, './public');
 
-app.use(express.static("publicPath"));
+app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
